cea270dc8f922343c6c7269181d6dd50a38df050
jocabispo <jljmendesmanutencao@gmail.com>
2022-03-23 16:11:47 -0300

    Adicionando funcionalodades ao botão cadastrar

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let Jmendes = {
     handleSubmit:(event)=>{
         event.preventDefault();
-    }
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+        }
+            send = false;
+            if(send){
+                form.submit();
+        
+        }
+    }   
 };
 
 let form = document.querySelector('.jmendes');
