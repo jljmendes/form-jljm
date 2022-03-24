6af5861d552b7e974f0ddde3eb600f447c262192
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-03-23 22:53:01 -0300

    Colocando aro vermelho nos form para mostrar o erro

--- script.js
+++ script.js
@@ -5,13 +5,57 @@ let Jmendes = {
 
         let inputs = form.querySelectorAll('input');
 
+        Jmendes.clearErros();
+
         for(let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
+            let check = Jmendes.checkInput(input);
+            if(check !== true){
+                send = false;
+                Jmendes.showError(input, check);
+            }
         }
             send = false;
             if(send){
                 form.submit();
         
+        }
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value == ''){
+                            return 'Campo não pode ser vazio.';
+                        }
+                        break;
+                        case 'min':
+
+                        break;
+                }
+            }
+        }
+
+        return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#ff0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add( 'error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErros:() =>{
+        let errorElements = document.querySelectorAll('.error');
+        for(let i = 0; i < errorElements.length; i++){
+            errorElements[i].remove();
         }
     }   
 };
